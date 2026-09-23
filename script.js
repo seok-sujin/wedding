28e576cc053b1986029cae3fb3f7478bffadcca1
seok-sujin <vy7357vy@gmail.com>
2026-09-23 16:03:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 // 🔻날짜 기입 (현재 2027년 1월 1일 12시 30분으로 설정되어있음)
-const WEDDING_YEAR = 2027; // 년도
-const WEDDING_MONTH = 1; // 월
-const WEDDING_DAY = 1;  // 일
+const WEDDING_YEAR = 2026; // 년도
+const WEDDING_MONTH = 11; // 월
+const WEDDING_DAY = 28;  // 일
 
-const WEDDING_HOUR = 12; //시
-const WEDDING_MINUTE = 30;  //분
+const WEDDING_HOUR = 13; //시
+const WEDDING_MINUTE = 00;  //분
 const WEDDING_DATE = new Date(
   WEDDING_YEAR,
   WEDDING_MONTH - 1,
